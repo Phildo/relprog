d6ae148d5dd7d2d62be917a6d7400c40cab8c5be
Phildo <phildo211@gmail.com>
2017-11-10 10:36:16 -0600

    initital doodle

---
+++ src/scenes/nullScene.js
@@ -0,0 +1,28 @@
+var NullScene = function(game, stage)
+{
+  var self = this;
+
+  var canv = stage.canv;
+  var canvas = canv.canvas;
+  var ctx = canv.context;
+
+  self.ready = function()
+  {
+
+  };
+
+  self.tick = function()
+  {
+
+  };
+
+  self.draw = function()
+  {
+
+  };
+
+  self.cleanup = function()
+  {
+
+  };
+};
